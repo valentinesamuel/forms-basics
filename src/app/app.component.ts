d301575fb2822d4387157be8e52d87f65d9fc842
valentinesamuel <valentinesamuel2580@gmail.com>
2021-01-23 00:23:10 +0100

    (FEAT):The button is working properly noew

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { deflateRaw } from 'zlib';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'forms-basics';
+  @ViewChild('form') detialsForm: NgForm;
+
+
+  onSubmit(data: NgForm) {
+    console.log(data);
+
+  }
 }
