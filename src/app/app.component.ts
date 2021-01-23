33a8b23a9c9732860569ca1151c4c70e758a81e1
valentinesamuel <valentinesamuel2580@gmail.com>
2021-01-23 21:02:37 +0100

    (FIX):Tried to get the value of the checkbox so i can disable the button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('form') detialsForm: NgForm;
   defaultQuestion = "teacher"
-
+  agreed = 'yes';
   onSubmit() {
-    console.log(this.detialsForm.value.userData.username);
-    console.log(this.detialsForm.value.userData.email);
-    console.log(this.detialsForm.value.secret);
+    // console.log(this.detialsForm.value.userData.username);
+    // console.log(this.detialsForm.value.userData.email);
+    // console.log(this.detialsForm.value.secret);
+    console.log(this.detialsForm.value.flexRadioDefault);
 
   }
 }
