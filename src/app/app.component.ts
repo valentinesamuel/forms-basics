6ad11fa6ebafb58b48551c3a3ed9bde6814e8492
valentinesamuel <valentinesamuel2580@gmail.com>
2021-01-23 21:53:27 +0100

    (FEAT):Displaying the details below

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,30 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('form') detialsForm: NgForm;
-  defaultQuestion = "teacher"
-  agreed = 'yes';
+  defaultQuestion = "teacher";
+  user = {
+    userName: "",
+    mail: "",
+    secretQuestion: "",
+
+  }
+
   onSubmit() {
+    this.user = {
+      userName: this.detialsForm.value.userData.username,
+      mail: this.detialsForm.value.userData.email,
+      secretQuestion: this.detialsForm.value.secret,
+
+    }
+
     // console.log(this.detialsForm.value.userData.username);
     // console.log(this.detialsForm.value.userData.email);
     // console.log(this.detialsForm.value.secret);
-    console.log(this.detialsForm.value.flexRadioDefault);
+    if (!this.detialsForm.value.flexRadioDefault) {
+      alert('Please check the box to continue');
+    } else {
+      console.log(this.detialsForm.value.flexRadioDefault);
+    }
 
   }
 }
