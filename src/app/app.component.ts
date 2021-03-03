74a373003574fe1fb98c7a578f2a9f8d2c67fb79
valentinesamuel <valentinesamuel2580@gmail.com>
2021-03-03 13:20:24 -0800

    (FEAT): Practiced pipes with this forms project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  collection: string[] = ['a', 'b', 'c', 'd'];
+    amount = 21000;
   // @ViewChild('form') detialsForm: NgForm;
   reactiveForm: FormGroup;
   sent = false;
