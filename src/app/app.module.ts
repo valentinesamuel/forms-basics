86d43cce42f5431dde6930b88ad14b6fb85a6467
valentinesamuel <valentinesamuel2580@gmail.com>
2021-03-03 14:12:30 -0800

    (FEAT): Created a filter search pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { ShortenPipe } from "./shorten.pipe";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppointmentComponent,
-    ShortenPipe
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
